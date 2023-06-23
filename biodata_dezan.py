a5f1ce35068e18adadc15db0cfada1154ac50082
fermentedblood <137252902+fermentedblood@users.noreply.github.com>
2023-06-23 10:15:03 +0700

    Add files via upload

---
+++ biodata_dezan.py
@@ -0,0 +1,14 @@
+print ("=======================================")
+print ("program biodata sederhana dengan pyhton")
+print ("=======================================")
+
+nama = "isi dengan nama kamu"
+hobi = "isi dengan hobi kamu"
+alamat = "isi dengan alamat kamu"
+kota = "isi dengan kota kamu"
+
+print("nama lengkap :",nama)
+print("alamat       :",alamat, "\n       kota ",kota)
+print("hobi	    :",hobi)
+
+print ("=======================================")
